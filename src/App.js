52b7b84da3611a53352750490fac279360db6b1a
vikyij <vikkyij54@gmail.com>
2021-12-27 16:15:11 +0100

    retrieved data using apollo client

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { useQuery, gql } from '@apollo/client'
+
+export const AnimeList = gql`
+  {
+    Page {
+      media {
+        siteUrl
+        title {
+          english
+          native
+        }
+        description
+        coverImage {
+          medium
+        }
+        bannerImage
+        volumes
+        episodes
+      }
+    }
+  }
+`
 
 function App() {
+  const { loading, error, data } = useQuery(AnimeList)
+
+  if (loading) return <> Loading</>
+  if (error) return <>{JSON.stringify(error)}</>
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h1> 🐈 Anime List </h1>
+      <hr width='80%' />
+      {data?.Page?.media.map((anime, id) => (
+        <>
+          <div className='card' key={id}>
+            <img src={anime.coverImage.medium} alt='anime img' />
+            <div>
+              <h1>{anime.title.english} </h1>
+              <div className='episodes'>
+                Episodes <b>{anime.episodes} </b>
+              </div>
+              <div
+                dangerouslySetInnerHTML={{ __html: anime.description }}
+              ></div>
+            </div>
+          </div>
+          <hr width='75%' />
+        </>
+      ))}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -3,13 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
+
+const client = new ApolloClient({
+  uri: 'https://graphql.anilist.co/',
+  cache: new InMemoryCache(),
+})
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
+  </ApolloProvider>,
   document.getElementById('root')
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
